85dd9723b91ccef79b50dc8ff9dde6fb315e72a8
Richard Yu <vipzhicheng@gmail.com>
2020-07-10 14:02:08 +0800

    fix: hook file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { Utils } from '@semo/core'
-
-export const handler = () => {
-  Utils.error('Plugin default handler placeholder triggered, means no default action defined.')  
-}
+export = {}
